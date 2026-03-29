3d09aac6bb148a6f7330675cd3767d4abfdbd618
sandeepa-prabashwara <ict22025@std.uwu.ac.lk>
2026-03-29 20:33:35 +0530

    Remove nested folder

--- AgriLanka-mewan01/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './common/Header';
-export { default as StatCard } from './common/StatCard';
-export { default as CropCard } from './common/CropCard';
-export { default as OrderCard } from './common/OrderCard';
